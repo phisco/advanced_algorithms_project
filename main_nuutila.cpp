322f5e27aec6e07b72eb265dfffaf8b06c70c982
Emanuele <emanuele.ricciardelli@mail.polimi.it>
2018-07-01 17:06:04 +0200

    Modificato CMakeLists e main di Nuutila

--- main_nuutila.cpp
+++ main_nuutila.cpp
@@ -25,7 +25,19 @@ typedef typename property_map<Graph, vertex_index_t>::type IndexMap;
 
 int main(int, char*[])
 {
+    enum { A, B, C, D, E, F, G, H, I, N };
+    const int num_nodes = N;
+    const char* name = "ABCDEFGHI";
+
+    // writing out the edges in the graph
     typedef std::pair<int, int> Edge;
+    Edge edge_array[] =
+            { Edge(A,B), Edge(B,A), Edge(A,C), Edge(C,A), Edge(B,D), Edge(C,D),
+              Edge(E,C), Edge(E,F), Edge(F,D), Edge(D,F), Edge(E,H), Edge(H,F), Edge(H,G), Edge(G,E),
+              Edge(I,G), Edge(I,H), Edge(I,I)};
+    const int num_edges = sizeof(edge_array)/sizeof(edge_array[0]);
+
+    /*typedef std::pair<int, int> Edge;
 
     const int num_nodes = 8;
 
@@ -41,7 +53,7 @@ int main(int, char*[])
                           Edge(H, A), Edge(H, G),
                           Edge(I, L),
                           Edge(M, N), Edge(N, M)
-    };
+    };*/
 
     int num_arcs = sizeof(edge_array) / sizeof(Edge);
     Graph g(edge_array, edge_array + num_arcs, num_nodes);
