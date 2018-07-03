3c96f4c6301d6834abd723e555eac4caf10c557a
Philippe scorsolini <pscorso93@gmail.com>
2018-07-03 21:36:13 +0200

    parametrized percentage

--- generate_graph.sh
+++ generate_graph.sh
@@ -1,11 +1,11 @@
-if [ $# != 4 ]
+if [ $# != 5 ]
 then
-    echo "help: generate_graph.sh root_dir min step max"
+    echo "help: generate_graph.sh root_dir_graphs_files min_nodes step max_nodes percentage_of_edges"
     exit 1
 fi
 mkdir $1
 for i in `seq $2 $3 $4 ` 
 do
     echo $i
-    ./cmake-build-debug/generate_graph $i 0.002 > $1/$i.xml
+    ./cmake-build-debug/generate_graph $i 0.002 > $1/$i\_$5.xml
 done
