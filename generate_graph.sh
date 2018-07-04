14cbf1052bab62a62344303679fc82d4951d1ef3
Philippe scorsolini <pscorso93@gmail.com>
2018-07-04 10:37:51 +0200

    added number of edges

--- generate_graph.sh
+++ generate_graph.sh
@@ -8,5 +8,13 @@ mkdir -p $1
 for i in `seq $2 $3 $4 `
 do
     echo $i
-    ./cmake-build-debug/generate_graph $i $5 > $1/$i\_$5.xml
+    ./cmake-build-debug/generate_graph $i $5 > tmp
+    edges=`tail tmp | awk '{print $2}' | grep "e[0-9]\+" | grep -o "[0-9]\+" | tail -n 1`
+    if [ -z $edges ]
+    then
+        edges=0
+    else
+        edges=`expr $edges + 1`
+    fi
+    mv tmp $1/$i\_$5\_$edges.xml
 done
