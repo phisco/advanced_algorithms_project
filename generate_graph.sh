edc479829d4e895dccef571569bc9bb3e92d72a1
Philippe scorsolini <pscorso93@gmail.com>
2018-07-06 12:14:17 +0200

    timings and notbooks

--- generate_graph.sh
+++ generate_graph.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 if [ $# != 5 ]
 then
-    echo "help: generate_graph.sh root_dir_graphs_files min_nodes step max_nodes percentage_of_edges"
+    echo "help: generate_graph.sh root_dir_graphs_files min_nodes step max_nodes step_edges"
     exit 1
 fi
 mkdir -p $1
-for i in `seq $2 $3 $4 `
+for nodes in `seq $2 $3 $4 `
 do
-    echo $i
-    ./cmake-build-debug/generate_graph $i $5 > tmp
-    edges=`tail tmp | awk '{print $2}' | grep "e[0-9]\+" | grep -o "[0-9]\+" | tail -n 1`
-    if [ -z $edges ]
-    then
-        edges=0
-    else
-        edges=`expr $edges + 1`
-    fi
-    mv tmp $1/$i\_$5\_$edges.xml
+    max=`expr $nodes \* \( $nodes - 1 \)`
+    echo $max
+    for e in `seq 0 $5 $max`
+    do
+        p=`echo "$e / $max" | bc -l | awk '{printf "%f", $0}'`
+        echo $nodes $p $e
+        ./cmake-build-debug/generate_graph $nodes $p > tmp
+        edges=`tail tmp | awk '{print $2}' | grep "e[0-9]\+" | grep -o "[0-9]\+" | tail -n 1`
+        if [ -z $edges ]
+        then
+            edges=0
+        else
+            edges=`expr $edges + 1`
+        fi
+        mv tmp $1/$nodes\_$p\_$edges.xml
+    done;
 done
