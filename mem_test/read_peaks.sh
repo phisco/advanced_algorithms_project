366b068fb5a6271420e7e32c720e3ff9dc809d90
Philippe scorsolini <pscorso93@gmail.com>
2018-07-22 17:48:45 +0200

    peaks for chain and  quasi_connected_graph

--- mem_test/read_peaks.sh
+++ mem_test/read_peaks.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh 
-for f in `ls $1`
+for f in `ls $1|grep massif.out`
 do
     echo $f >> res.txt
     cat $1/$f | grep -I -B 4 -A 4 peak | tee >(awk '/mem_heap_B=/{print $1}' >>res.txt) >(awk '/mem_heap_extra_B=/{print $1}' >> res.txt) >(awk '/mem_stacks_B=/{print $1}' >> res.txt)
 done
-python read_peaks.py
+python ../read_peaks.py
 
